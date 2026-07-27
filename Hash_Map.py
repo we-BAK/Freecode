27ef65e970547694fac69f4ba9a5ae7b1797f6d4
we_BAK <hailemichaelkidist@gmail.com>
2026-07-27 15:29:20 +0300

    HashMap

---
+++ Hash_Map.py
@@ -0,0 +1,31 @@
+class HashTable:
+    def __init__(self):
+        self.collection = {}
+
+    def hash(self, key: str) -> int:
+        return sum(ord(char) for char in key)
+
+    def add(self, key: str, value):
+        hashed_key = self.hash(key)
+
+        if hashed_key not in self.collection:
+            self.collection[hashed_key] = {}
+
+        self.collection[hashed_key][key] = value
+
+    def remove(self, key: str):
+        hashed_key = self.hash(key)
+
+        if hashed_key in self.collection and key in self.collection[hashed_key]:
+            del self.collection[hashed_key][key]
+
+            if not self.collection[hashed_key]:
+                del self.collection[hashed_key]
+
+    def lookup(self, key: str):
+        hashed_key = self.hash(key)
+
+        if hashed_key in self.collection and key in self.collection[hashed_key]:
+            return self.collection[hashed_key][key]
+
+        return None
