e6b6f15032e6c24b66b7fa134e9eab6918a2d5da
we_BAK <hailemichaelkidist@gmail.com>
2026-08-05 21:43:22 +0300

    Unitconverter

--- UnitConverter.py
+++ UnitConverter.py
@@ -1,9 +1,7 @@
 def meters_to_feet(meters):
     return meters * 3.28084
-
 def kg_to_lbs(kg):
     return kg * 2.20462
-
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9 / 5) + 32
 print("=== 🔄 Simple Unit Converter ===")
