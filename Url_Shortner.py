98ade5d8dc7104f3d38419fc256d447174854031
we_BAK <hailemichaelkidist@gmail.com>
2026-08-16 14:01:16 +0300

    second fun

--- Url_Shortner.py
+++ Url_Shortner.py
@@ -4,3 +4,9 @@ url_map={}
 def generate_key(length=6):
     characters=string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
+def shorten_url(long_url):
+    key=generate_key()
+    while key in url_map:
+        key=generate_key()
+    url_map[key]=long_url
+    return key
